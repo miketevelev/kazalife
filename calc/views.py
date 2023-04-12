017ad6468041fb6361f101f5ec11964b528d7ef5
Mike Tevelev <miketevelev@gmail.com>
2023-04-12 18:16:00 +0300

    render currency

--- calc/views.py
+++ calc/views.py
@@ -9,4 +9,28 @@ def index(request):
     )
 
 def currency():
-    return 5,5
+    import requests
+    from bs4 import BeautifulSoup
+
+    currency_sum = 0.0
+
+    url = "https://cbr.ru/currency_base/daily/"
+    data = requests.get(url).text
+    soup = BeautifulSoup(data, 'html.parser')
+    tables = soup.find_all('table')
+    table = soup.find('table', class_='data')
+
+    for row in table.tbody.find_all('tr'):
+        columns = row.find_all('td')
+
+        if (columns != []):
+            code = columns[0].text.strip()
+            letter = columns[1].text.strip()
+            count = columns[2].text.strip()
+            currency = columns[3].text.strip()
+            number = columns[4].text.strip()
+
+            if code == '398' and letter == 'KZT':
+                currency_sum = number
+
+    return currency_sum
